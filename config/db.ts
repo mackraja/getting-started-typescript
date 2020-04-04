5413aeeba1877daab1b563eb4432962e1bc3b3e4
mackraja <montykhanna007@hotmail.com>
2020-04-05 02:12:34 +0530

    :zap: db

--- config/db.ts
+++ config/db.ts
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "Bohemia00(",
+    password: process.env.DB_PASSWORD || "Bohemia00&",
     database: process.env.DB_DATABASE || "gettingStartedTypeScript",
     host: process.env.DB_HOST || "localhost",
     dialect: process.env.DB_DIALECT || "mysql",
