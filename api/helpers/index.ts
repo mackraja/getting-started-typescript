a06063e12a6c1bd944d187cec434ed35b527ee28
mackraja <montykhanna007@hotmail.com>
2020-04-28 12:40:01 +0530

    Removed unwanted code

--- api/helpers/index.ts
+++ api/helpers/index.ts
@@ -1,12 +1,8 @@
 /**
  * @author: Monty Khanna
  */
-// import authenticate from './authenticate'; // eslint-disable-line
 import i18n from './i18n'; // eslint-disable-line
-import hashPassword from './hashPassword'; // eslint-disable-line
 
 export {
-  // authenticate,
   i18n,
-  hashPassword,
 };
